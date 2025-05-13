5bedb60697b8c90de011862ff5add4e0f505db8a
Mihaitzacapra <mihai.morari0811@gmail.com>
2025-05-13 21:11:14 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("giftcardForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  document.getElementById("confirmare").innerText = "Giftcardul a fost simulat cu succes! (doar demo)";
+});
